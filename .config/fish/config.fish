bbab6710684704dab3812aea1a67fe8b48044a66
David Tennander <date@MacBook-Pro-som-tillhor-David.local>
2024-10-28 13:11:50 +0100

    Make it work with fish out of the box

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,4 @@ end
 
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/daten/google-cloud-sdk/path.fish.inc' ]; . '/Users/daten/google-cloud-sdk/path.fish.inc'; end
+eval "$(/opt/homebrew/bin/brew shellenv)"
